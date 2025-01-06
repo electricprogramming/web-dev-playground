5a76427efac962680cdd4622847bcd7104d1843b
electricprogramming <yeungj316@gmail.com>
2025-01-06 17:17:40 -0500

    percent not decimal

--- src/pages/JavaScript/script.js
+++ src/pages/JavaScript/script.js
@@ -13,17 +13,17 @@ const divider = document.getElementById('divider');
 let dividerDragging = false;
 divider.addEventListener('mousedown', () => {
   dividerDragging = true;
+  document.addEventListener('mouseup', () => {
+    dividerDragging = false;
+  }, { once: true });
 });
 document.addEventListener('mousemove', (e) => {
   if (dividerDragging) {
     const x = e.pageX;
-    const percent = x / window.innerWidth;
+    const percent = x / window.innerWidth * 100;
     console.log(x, percent, window.innerWidth);
     if (percent >= 30 && percent <= 90) {
       divider.style.left = `${percent}vw`;
     }
   }
-});
-divider.addEventListener('mouseup', () => {
-  dividerDragging = false;
 });
