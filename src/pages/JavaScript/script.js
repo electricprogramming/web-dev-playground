57feebc408073f4047dbcb07caa6004185b5b5dc
electricprogramming <yeungj316@gmail.com>
2025-01-06 17:11:54 -0500

    ok

--- src/pages/JavaScript/script.js
+++ src/pages/JavaScript/script.js
@@ -1,3 +1,4 @@
+import clamp from '/src/utils/clamp.js';
 CodeMirror.fromTextArea(document.getElementById('editor'), {
   mode: 'javascript',
   lineNumbers: true,
@@ -7,4 +8,21 @@ CodeMirror.fromTextArea(document.getElementById('editor'), {
   matchBrackets: true,
   autoCloseBrackets: true,
   lineWrapping: false
+});
+const divider = document.getElementById('divider');
+let dividerDragging = false;
+divider.addEventListener('mousedown', () => {
+  dividerDragging = true;
+});
+document.addEventListener('mousemove', (e) => {
+  if (dividerDragging) {
+    const x = e.pageX;
+    const percent = x / window.innerWidth;
+    if (percent >= 30 && percent <= 90) {
+      divider.style.left = `${percent}vw`;
+    }
+  }
+});
+divider.addEventListener('mouseup', () => {
+  dividerDragging = false;
 });
