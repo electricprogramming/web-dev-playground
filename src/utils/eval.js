ee02f2e3c56823d030c14de366b6f1f1bc92efd7
electricprogramming <yeungj316@gmail.com>
2025-01-12 18:28:58 -0500

    newlines

--- src/utils/eval.js
+++ src/utils/eval.js
@@ -8,25 +8,37 @@ export default function _eval(code) {
     console.log = function(...data) {
       const log = document.createElement('div');
       log.classList.add('log');
-      log.textContent = data.join('  ');
+      text = data.join('  ');
+      log.innerHTML = text
+        .replaceAll('\n', '<br>')
+        .replaceAll('<', '&lt;')
       document.getElementById('console').appendChild(log);
     }
     console.error = function(...data) {
       const err = document.createElement('div');
       err.classList.add('error');
-      err.textContent = data.join('  ');
+      text = data.join('  ');
+      err.innerHTML = text
+        .replaceAll('\n', '<br>')
+        .replaceAll('<', '&lt;')
       document.getElementById('console').appendChild(err);
     }
     console.warn = function(...data) {
       const warn = document.createElement('div');
       warn.classList.add('warn');
-      warn.textContent = data.join('  ');
+      text = data.join('  ');
+      warn.innerHTML = text
+        .replaceAll('\n', '<br>')
+        .replaceAll('<', '&lt;')
       document.getElementById('console').appendChild(warn);
     }
     console.info = function(...data) {
       const info = document.createElement('div');
       info.classList.add('info');
-      info.textContent = data.join('  ');
+      text = data.join('  ');
+      info.innerHTML = text
+        .replaceAll('\n', '<br>')
+        .replaceAll('<', '&lt;')
       document.getElementById('console').appendChild(info);
     }
     return eval(${JSON.stringify(code)});
