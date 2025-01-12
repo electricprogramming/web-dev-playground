a658e36cb4963be92c85ab6da12b7de0db7101cc
electricprogramming <yeungj316@gmail.com>
2025-01-12 15:33:49 -0500

    BAM

--- src/utils/eval.js
+++ src/utils/eval.js
@@ -5,8 +5,29 @@
  */
 export default function _eval(code) {
   const func = new Function(`
-    console.log = function() {
-      // custom log; customize other console funcs to work in console element
+    console.log = function(...data) {
+      const log = document.createElement('div');
+      log.classList.add('log');
+      log.textContent = data.join('  ');
+      document.getElementById('console').appendChild(log);
+    }
+    console.error = function(...data) {
+      const err = document.createElement('div');
+      err.classList.add('error');
+      err.textContent = data.join('  ');
+      document.getElementById('console').appendChild(err);
+    }
+    console.warn = function(...data) {
+      const warn = document.createElement('div');
+      warn.classList.add('warn');
+      warn.textContent = data.join('  ');
+      document.getElementById('console').appendChild(warn);
+    }
+    console.info = function(...data) {
+      const info = document.createElement('div');
+      info.classList.add('info');
+      info.textContent = data.join('  ');
+      document.getElementById('console').appendChild(info);
     }
     return eval(${JSON.stringify(code)});
   `);
