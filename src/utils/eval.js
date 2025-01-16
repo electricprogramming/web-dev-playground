93b5978057062511046beba6aa3211f82a6a9923
electricprogramming <yeungj316@gmail.com>
2025-01-16 16:43:44 -0500

    log +

--- src/utils/eval.js
+++ src/utils/eval.js
@@ -8,6 +8,35 @@ export default function _eval(code) {
     console.log = function(...data) {
       const log = document.createElement('div');
       log.classList.add('log');
+      let htmlData;
+      data.forEach(item => {
+        if (typeof item === 'object') {
+          try {
+            item = JSON.stringify(item, null, 2);
+          } catch{}
+        }
+        if (typeof item === 'boolean') {
+          htmlData += \`<span class="boolean">\${item}</span>\`;
+        }
+        if (typeof item === 'number' || typeof item === 'bigint') {
+          htmlData += \`<span class="number">\${item}</span>\`;
+        }
+        if (typeof item === 'undefined') {
+          htmlData += '<span class="undefined">undefined</span>;
+        }
+        if (typeof item === 'function') {
+          item = item.toString();
+        }
+        if (typeof item === 'symbol') {
+          item = item.description;
+        }
+        if (typeof item === 'string') {
+          htmlData += item
+            .replaceAll('<', '&lt;')
+            .replaceAll('\\n', '<br>');
+        }
+        htmlData += '  ';
+      });
       text = data.join('  ');
       log.innerHTML = text
         .replaceAll('<', '&lt;')
