49b46184260467db1c12de6de60445bbe4c55f04
electricprogramming <yeungj316@gmail.com>
2025-01-16 16:56:41 -0500

    log + global

--- src/utils/eval.js
+++ src/utils/eval.js
@@ -37,35 +37,112 @@ export default function _eval(code) {
         }
         htmlData += '  ';
       });
-      console.dir(htmlData);
       log.innerHTML = htmlData;
       document.getElementById('console').appendChild(log);
     }
     console.error = function(...data) {
       const err = document.createElement('div');
       err.classList.add('error');
-      text = data.join('  ');
-      err.innerHTML = text
-        .replaceAll('<', '&lt;')
-        .replaceAll('\\n', '<br>');
+      let htmlData = '';
+      data.forEach(item => {
+        if (typeof item === 'object') {
+          try {
+            item = JSON.stringify(item, null, 2);
+          } catch{}
+        }
+        if (typeof item === 'boolean') {
+          htmlData += \`<span class="boolean">\${item}</span>\`;
+        }
+        if (typeof item === 'number' || typeof item === 'bigint') {
+          htmlData += \`<span class="number">\${item}</span>\`;
+        }
+        if (typeof item === 'undefined') {
+          htmlData += '<span class="undefined">undefined</span>';
+        }
+        if (typeof item === 'function') {
+          item = item.toString();
+        }
+        if (typeof item === 'symbol') {
+          item = item.description;
+        }
+        if (typeof item === 'string') {
+          htmlData += item
+            .replaceAll('<', '&lt;')
+            .replaceAll('\\n', '<br>');
+        }
+        htmlData += '  ';
+      });
+      err.innerHTML = htmlData;
       document.getElementById('console').appendChild(err);
     }
     console.warn = function(...data) {
       const warn = document.createElement('div');
       warn.classList.add('warn');
-      text = data.join('  ');
-      warn.innerHTML = text
-        .replaceAll('<', '&lt;')
-        .replaceAll('\\n', '<br>');
+      let htmlData = '';
+      data.forEach(item => {
+        if (typeof item === 'object') {
+          try {
+            item = JSON.stringify(item, null, 2);
+          } catch{}
+        }
+        if (typeof item === 'boolean') {
+          htmlData += \`<span class="boolean">\${item}</span>\`;
+        }
+        if (typeof item === 'number' || typeof item === 'bigint') {
+          htmlData += \`<span class="number">\${item}</span>\`;
+        }
+        if (typeof item === 'undefined') {
+          htmlData += '<span class="undefined">undefined</span>';
+        }
+        if (typeof item === 'function') {
+          item = item.toString();
+        }
+        if (typeof item === 'symbol') {
+          item = item.description;
+        }
+        if (typeof item === 'string') {
+          htmlData += item
+            .replaceAll('<', '&lt;')
+            .replaceAll('\\n', '<br>');
+        }
+        htmlData += '  ';
+      });
+      warn.innerHTML = htmlData;
       document.getElementById('console').appendChild(warn);
     }
     console.info = function(...data) {
       const info = document.createElement('div');
       info.classList.add('info');
-      text = data.join('  ');
-      info.innerHTML = text
-        .replaceAll('<', '&lt;')
-        .replaceAll('\\n', '<br>');
+      let htmlData = '';
+      data.forEach(item => {
+        if (typeof item === 'object') {
+          try {
+            item = JSON.stringify(item, null, 2);
+          } catch{}
+        }
+        if (typeof item === 'boolean') {
+          htmlData += \`<span class="boolean">\${item}</span>\`;
+        }
+        if (typeof item === 'number' || typeof item === 'bigint') {
+          htmlData += \`<span class="number">\${item}</span>\`;
+        }
+        if (typeof item === 'undefined') {
+          htmlData += '<span class="undefined">undefined</span>';
+        }
+        if (typeof item === 'function') {
+          item = item.toString();
+        }
+        if (typeof item === 'symbol') {
+          item = item.description;
+        }
+        if (typeof item === 'string') {
+          htmlData += item
+            .replaceAll('<', '&lt;')
+            .replaceAll('\\n', '<br>');
+        }
+        htmlData += '  ';
+      });
+      info.innerHTML = htmlData;
       document.getElementById('console').appendChild(info);
     }
     return eval(${JSON.stringify(code)});
