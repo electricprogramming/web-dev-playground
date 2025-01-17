f6d4e22d6e0bd1455e42008a45efced0c94817dc
electricprogramming <yeungj316@gmail.com>
2025-01-17 16:32:11 -0500

    YEAH

--- src/utils/eval.js
+++ src/utils/eval.js
@@ -104,40 +104,43 @@ export default function _eval(code) {
       warn.classList.add('warn');
       let htmlData = '';
       data.forEach(item => {
-        if (item instanceof Error) {
-          item = item.toString();
+        function getAddlHtml() {
+          if (item instanceof Error) {
+            item = item.toString();
+          }
+          if (item === null) {
+            return '<span class="undefd">null</span>';
+          }
+          if (typeof item === 'object') {
+            try {
+              item = JSON.stringify(item, null, 2);
+            } catch {}
+          }
+          if (typeof item === 'boolean') {
+            return \`<span class="boolean">\${item}</span>\`;
+          }
+          if (typeof item === 'number') {
+            return \`<span class="number">\${item}</span>\`;
+          }
+          if (typeof item === 'bigint') {
+            return \`<span class="number">\${item}n</span>\`
+          }
+          if (typeof item === undefined) {
+            return '<span class="undefd">undefined</span>';
+          }
+          if (typeof item === 'function') {
+            item = item.toString();
+          }
+          if (typeof item === 'symbol') {
+            item = item.description;
+          }
+          if (typeof item === 'string') {
+            return item
+              .replaceAll('<', '&lt;')
+              .replaceAll('\\n', '<br>');
+          }
         }
-        if (typeof item === 'object') {
-          try {
-            item = JSON.stringify(item, null, 2);
-          } catch{}
-        }
-        if (typeof item === 'boolean') {
-          htmlData += \`<span class="boolean">\${item}</span>\`;
-        }
-        if (typeof item === 'number') {
-          htmlData += \`<span class="number">\${item}</span>\`;
-        }
-        if (typeof item === 'bigint') {
-          htmlData += \`<span class="number">\${item}n</span>\`
-        }
-        if (typeof item === undefined) {
-          htmlData += '<span class="undefd">undefined</span>';
-        }
-        if (item === 'null') {
-          htmlData += '<span class="undefd">null</span>';
-        }
-        if (typeof item === 'function') {
-          item = item.toString();
-        }
-        if (typeof item === 'symbol') {
-          item = item.description;
-        }
-        if (typeof item === 'string') {
-          htmlData += item
-            .replaceAll('<', '&lt;')
-            .replaceAll('\\n', '<br>');
-        }
+        htmlData += getAddlHtml();
         htmlData += '  ';
       });
       warn.innerHTML = htmlData;
@@ -148,40 +151,43 @@ export default function _eval(code) {
       info.classList.add('info');
       let htmlData = '';
       data.forEach(item => {
-        if (item instanceof Error) {
-          item = item.toString();
+        function getAddlHtml() {
+          if (item instanceof Error) {
+            item = item.toString();
+          }
+          if (item === null) {
+            return '<span class="undefd">null</span>';
+          }
+          if (typeof item === 'object') {
+            try {
+              item = JSON.stringify(item, null, 2);
+            } catch {}
+          }
+          if (typeof item === 'boolean') {
+            return \`<span class="boolean">\${item}</span>\`;
+          }
+          if (typeof item === 'number') {
+            return \`<span class="number">\${item}</span>\`;
+          }
+          if (typeof item === 'bigint') {
+            return \`<span class="number">\${item}n</span>\`
+          }
+          if (typeof item === undefined) {
+            return '<span class="undefd">undefined</span>';
+          }
+          if (typeof item === 'function') {
+            item = item.toString();
+          }
+          if (typeof item === 'symbol') {
+            item = item.description;
+          }
+          if (typeof item === 'string') {
+            return item
+              .replaceAll('<', '&lt;')
+              .replaceAll('\\n', '<br>');
+          }
         }
-        if (typeof item === 'object') {
-          try {
-            item = JSON.stringify(item, null, 2);
-          } catch{}
-        }
-        if (typeof item === 'boolean') {
-          htmlData += \`<span class="boolean">\${item}</span>\`;
-        }
-        if (typeof item === 'number') {
-          htmlData += \`<span class="number">\${item}</span>\`;
-        }
-        if (typeof item === 'bigint') {
-          htmlData += \`<span class="number">\${item}n</span>\`
-        }
-        if (typeof item === undefined) {
-          htmlData += '<span class="undefd">undefined</span>';
-        }
-        if (item === 'null') {
-          htmlData += '<span class="undefd">null</span>';
-        }
-        if (typeof item === 'function') {
-          item = item.toString();
-        }
-        if (typeof item === 'symbol') {
-          item = item.description;
-        }
-        if (typeof item === 'string') {
-          htmlData += item
-            .replaceAll('<', '&lt;')
-            .replaceAll('\\n', '<br>');
-        }
+        htmlData += getAddlHtml();
         htmlData += '  ';
       });
       info.innerHTML = htmlData;
