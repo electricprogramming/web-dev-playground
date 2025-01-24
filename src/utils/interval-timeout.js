6cf1759eaa2bf51865740017e54be5caff67e398
electricprogramming <yeungj316@gmail.com>
2025-01-23 19:55:32 -0500

    still need play btn asset and div

--- src/utils/interval-timeout.js
+++ src/utils/interval-timeout.js
@@ -1,3 +1,5 @@
+// Creates setInterval and setTimeout functions that can be used within the _eval file so that intervals and timeouts
+// created within the playground REPL using setInterval or setTimeout can be cleared each time the code is run.
 const intervals = [], timeouts = [];
 const ogInterval = setInterval, ogTimeout = setTimeout;
 const newInterval = function(handler, timeout = undefined, ...args) {
