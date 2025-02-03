cd27e942cad146e20dd905bb57d7f953d70ddacc
electricprogramming <yeungj316@gmail.com>
2025-02-03 11:32:39 -0500

    revertify

--- src/utils/global-mods/object-for-each.js
+++
@@ -1,5 +0,0 @@
-Object.prototype.forEach = function(callback, thisArg = undefined) {
-  Object.entries(this).forEach(([key, value], index) => {
-    callback.call(thisArg, key, value, index, this);
-  });
-}
