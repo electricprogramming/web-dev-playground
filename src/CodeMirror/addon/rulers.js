0eded55e10bf4e0fc6eeb872d7848d6b7f7d8705
electricprogramming <yeungj316@gmail.com>
2025-03-11 10:32:48 -0400

    style.bottom

--- src/CodeMirror/addon/rulers.js
+++ src/CodeMirror/addon/rulers.js
@@ -15,7 +15,7 @@ function addRulers(editor, frequency) {
 
     for (let j = frequency; j <= lineLength; j += frequency) {
       let ruler = createRuler(j, charWidth, textHeight);
-      rulerWidgets.push(editor.addLineWidget(i - 1, ruler));
+      rulerWidgets.push(editor.addLineWidget(i, ruler));
     }
   }
 }
@@ -24,26 +24,12 @@ function createRuler(position, charWidth, textHeight) {
   let ruler = document.createElement('div');
   ruler.style.position = 'absolute';
   ruler.style.left = `${4 + (position * charWidth)}px`;
+  ruler.style.bottom = '0';
   ruler.style.width = '1px';
   ruler.style.height = `${textHeight}px`;
   ruler.classList.add('CodeMirror-ruler');
 
   return ruler;
-}
-
-function getCharWidth(editor) {
-  const editorEl = editor.getWrapperElement();
-  const editorStyle = window.getComputedStyle(editorEl);
-  let testChar = document.createElement('div');
-  testChar.style.position = 'absolute';
-  testChar.style.visibility = 'hidden';
-  testChar.style.fontFamily = editorStyle.fontFamily || 'monospace';
-  testChar.style.fontSize = editorStyle.fontSize || editor.defaultTextHeight();
-  testChar.textContent = 'X';
-  document.body.appendChild(testChar);
-  let charWidth = testChar.offsetWidth;
-  document.body.removeChild(testChar);
-  return charWidth;
 }
 
 CodeMirror.defineOption('rulers', { on: false }, function(cm, val) {
