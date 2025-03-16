cb33e9e5782c1b3a44928c5bd16cc0c0ecbb17a2
electricprogramming <yeungj316@gmail.com>
2025-03-16 17:01:47 -0400

    simpel

--- src/webpages/JavaScript/script.js
+++ src/webpages/JavaScript/script.js
@@ -20,7 +20,7 @@ const editor = CodeMirror.fromTextArea(document.getElementById('editor-textarea'
   foldGutter: true,
   rulers: true,
   gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-  scrollbarStyle: CodeMirror.scrollbarModel.simple,
+  scrollbarStyle: 'simple',
   lineWrapping: false,
   foldOptions: {
     widget: function() {
