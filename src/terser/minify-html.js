10a96e56f97b3088254a44eb26738f1365b399bc
electricprogramming <yeungj316@gmail.com>
2025-03-26 12:22:48 -0400

    terser (not quite qorking yet)

--- src/terser/minify-html.js
+++ src/terser/minify-html.js
@@ -4,8 +4,32 @@ the context of this project without making HTMLMinifier a global object.
 However, it may not function in an environment that does not support ESM.
 */
 
-// Original code is licensed under the MIT license -
-// https://cdn.jsdelivr.net/npm/html-minifier-terser@7.2.0/LICENSE
+// Original code is licensed under the MIT license, as follows:
+
+/*
+Copyright (c) 2010-2019 Juriy "kangax" Zaytsev
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 const _export = (function() {
   'use strict';
