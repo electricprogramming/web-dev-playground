70f15f25f6723b34898f438cddc144c0f9e04e15
electricprogramming <yeungj316@gmail.com>
2025-03-30 13:22:47 -0400

    oops

--- src/minify/index.js
+++ src/minify/index.js
@@ -2,5 +2,5 @@ import { minify_sync as _minifyJS } from './terser.js';
 import settings from './settings.js';
 
 export default function minifyJS(js_code) {
-  return _minifyJS(js_code, settings);
+  return _minifyJS(js_code, settings).code;
 }
